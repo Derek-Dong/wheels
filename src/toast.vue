e25e7a396354121d393fe7184a2daca7e199892c
Derek-Dong <Derek-Dong@foxmail.com>
2020-12-17 19:10:16 +0800

    实现三种动画

--- src/toast.vue
+++ src/toast.vue
@@ -92,28 +92,49 @@
   $font-size: 14px;
   $toast-min-height: 40px;
   $toast-bg: rgba(0, 0, 0, 0.75);
-  @keyframes fade-in {
+  @keyframes slide-up {
     0% {opacity: 0; transform: translateY(100%);}
     100% {opacity: 1;transform: translateY(0%);}
+  }
+  @keyframes slide-down {
+    0% {opacity: 0; transform: translateY(-100%);}
+    100% {opacity: 1;transform: translateY(0%);}
+  }
+  @keyframes fade-in {
+    0% {opacity: 0; }
+    100% {opacity: 1;}
   }
   .wrapper {
     position: fixed; 
     left: 50%;
     transform: translateX(-50%);
+    $animation-duration: 300ms;
     &.position-top{
       top: 0;
+      .toast {
+        border-top-left-radius: 0;
+        border-top-right-radius: 0;
+        animation: slide-down $animation-duration;
+      }
     }
     &.position-bottom{
       bottom: 0;
+      .toast {
+        border-bottom-left-radius: 0;
+        border-bottom-right-radius: 0;
+        animation: slide-up $animation-duration;
+      }
     }
     &.position-middle{
       top: 50%;
       transform: translate(-50%, -50%);
+      .toast {
+        animation: fade-in $animation-duration;
+      }
     }
   }
 
   .toast {
-    animation: fade-in 1s;
     font-size: $font-size; 
     min-height: $toast-min-height; 
     line-height: 1.8;
