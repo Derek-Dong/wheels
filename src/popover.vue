3fd2667c410fba9beba2ec62541346644c0ce911
Derek-Dong <Derek-Dong@foxmail.com>
2020-12-27 11:39:05 +0800

    解决抖动bug

--- src/popover.vue
+++ src/popover.vue
@@ -153,10 +153,12 @@
       }
       &::before {
         border-top-color: black;
+        border-bottom: none;
         top: 100%;
       }
       &::after {
         border-top-color: white;
+        border-bottom: none;
         top: calc(100% - 1px);
       }
     }
@@ -166,10 +168,12 @@
         left: 10px;
       }
       &::before {
+        border-top: none;
         border-bottom-color: black;
         bottom: 100%;
       }
       &::after {
+        border-top: none;
         border-bottom-color: white;
         bottom: calc(100% - 1px);
       }
@@ -183,10 +187,12 @@
       }
       &::before {
         border-left-color: black;
+        border-right: none;
         left: 100%;
       }
       &::after {
         border-left-color: white;
+        border-right: none;
         left: calc(100% - 1px);
       }
     }
@@ -198,10 +204,12 @@
       }
       &::before {
         border-right-color: black;
+        border-left: none;
         right: 100%;
       }
       &::after {
         border-right-color: white;
+        border-left: none;
         right: calc(100% - 1px);
       }
     }
