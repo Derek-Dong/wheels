09689222ee848536df19a26aa97b18827be60621
Derek-Dong <Derek-Dong@foxmail.com>
2020-12-28 19:41:17 +0800

    制作自己的官网

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  base: '/Wheels/',
+  title: '轮子UI',
+  description: '一个好用的UI框架',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/guide/' },
+      { text: 'External', link: 'https://google.com' },
+    ],
+    sidebar: [
+      {
+        title: '入门',
+        children: [
+          '/install/',
+          '/get-started/',
+        ]
+      },
+      {
+        title: '组件',
+        children: [
+          '/components/button',
+          '/components/tabs',
+          '/components/input',
+          '/components/grid',
+          '/components/layout',
+          '/components/toast',
+          '/components/tabs',
+        ]
+      },
+
+    ]
+  }
+} 
